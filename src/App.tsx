42ea92a021eae80eb4f752937c3b4394a4b6b227
tiavina-mika <tiavinamika@gmail.com>
2024-10-08 12:22:37 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchAutocomplete from './components/SearchAutocomplete';
+
+function App() {
+  return (
+    <div>
+      <h1>React Vite App</h1>
+      <SearchAutocomplete />
+    </div>
+  );
+}
+
+export default App;
